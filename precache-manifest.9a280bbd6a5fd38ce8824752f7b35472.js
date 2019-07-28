f91b5bba1d9edc502ec50b67f7cbfd1f224215be
ramyaperi <ramyaperi2@gmail.com>
2019-07-28 21:43:08 +0200

    Updates

---
+++ precache-manifest.9a280bbd6a5fd38ce8824752f7b35472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ef9f2058edb5cac41ba96dd04d4f28",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "0931d2197a5dde7340b2",
+    "url": "/weather_app/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "61a94edc392561a58bb6",
+    "url": "/weather_app/static/js/2.af908036.chunk.js"
+  },
+  {
+    "revision": "0931d2197a5dde7340b2",
+    "url": "/weather_app/static/js/main.c2c54fa3.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  }
+]);
